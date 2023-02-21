b5d7ef04986c9f7a315e40e4c6f6aeade2cddb63
davidclare <mr.d.clare@gmail.com>
2023-02-21 14:59:07 +0000

    next

--- app/src/server.ts
+++ app/src/server.ts
@@ -6,6 +6,24 @@ app.set("port", process.env.PORT || 3000);
 
 console.log("Hello World..................");
 
+process.on('SIGHUP', (signal) => {
+  console.log(`*^!@4=> Received event: ${ signal }`)
+})
+
+const closeGracefully = async (signal: NodeJS.Signals) => {
+  console.log(`*^!@4=> Received signal to terminate: ${ signal }`)
+
+  // @ts-ignore
+  await fastify.close()
+  // await db.close() if we have a db connection in this app
+  // await other things we should cleanup nicely
+  process.kill(process.pid, signal);
+}
+
+process.once('SIGINT', closeGracefully)
+
+process.once('SIGTERM', closeGracefully)
+
 app.use("/", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   try {
     res.status(200).send("ok..")
